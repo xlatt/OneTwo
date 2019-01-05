86d9b48402d2e90efb34636359a319453099b328
Daired <dairedos@hotmail.com>
2019-01-05 13:01:40 +0100

    Folder Cleanup 1#

---
+++ Assets/Scripts/Music.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Music : MonoBehaviour {
+
+	// Use this for initialization
+	void Awake () {
+        FindObjectOfType<AudioManager>().Stop();
+        switch (SceneManager.GetActiveScene().buildIndex) {
+            case 0: {
+                    FindObjectOfType<AudioManager>().Play("MainSoundtrack1");
+                    break;
+                }
+
+            case 1: {
+                    FindObjectOfType<AudioManager>().Play("MainSoundtrack2");
+                    break;
+                }
+            default: break;
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
