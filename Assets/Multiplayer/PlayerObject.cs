07f428c1f63c9a082ad7c32d2273848e31765726
xlatt <burianpato@protonmail.com>
2019-01-15 18:23:37 +0100

    Add NetworkManager and PlayerObject for MP into MasterMenu scenne

---
+++ Assets/Multiplayer/PlayerObject.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerObject : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Instantiate(playerUnitPrefab);
+	}
+
+    public GameObject playerUnitPrefab;
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
