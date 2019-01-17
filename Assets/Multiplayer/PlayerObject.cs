20f3c13657da0b1811fa54e768f58627018a9de9
xlatt <burianpato@protonmail.com>
2019-01-17 21:52:01 +0100

    Add PlayerUnit and PlayerObject
    These objects are spawned at the start of the MP match.

--- Assets/Multiplayer/PlayerObject.cs
+++ Assets/Multiplayer/PlayerObject.cs
@@ -1,17 +1,38 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 
-public class PlayerObject : MonoBehaviour {
+public class PlayerObject : NetworkBehaviour {
 
 	// Use this for initialization
 	void Start () {
-        Instantiate(playerUnitPrefab);
+        // is this my own local player object?
+        if (!isLocalPlayer) {
+            // nope, someone else
+            return;
+        }
+
+        // I am invisible so spawn something physical
+        Instantiate(PlayerUnitPrefab);
+
+        Debug.Log("PlayerObject::Start - Spawned ");
 	}
 
-    public GameObject playerUnitPrefab;
+    public GameObject PlayerUnitPrefab;
 	// Update is called once per frame
 	void Update () {
-		
+		// This runs on everyone's computer weather or not they own this object
 	}
+
+    ///////////// COMMANDS
+    /// Commands are executed only on server
+    [Command]
+    void CmdSpawnMyUnit() {
+        Debug.Log("SRV CMD - Spawning player");
+        GameObject go = Instantiate(PlayerUnitPrefab);
+        // Object exists on server, propagate to clients
+        NetworkServer.Spawn(go);
+        Debug.Log("SRV CMD - Player spawned");
+    }
 }
