d40c9fb0c0df87fa3de45a87ccfcec57c53340ff
Daired <dairedos@hotmail.com>
2019-01-17 20:30:22 +0100

    Find Resource exception fix and class cleanup

--- Assets/Scripts/inGameEntities/ActionResource.cs
+++ Assets/Scripts/inGameEntities/ActionResource.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FindResource : MonoBehaviour {
+public class ActionResource : MonoBehaviour {
 
     public int resourceCount = 0;
     private int assetDecreaser = 10;
@@ -13,7 +13,7 @@ public class FindResource : MonoBehaviour {
     private GameObject parentUnit;
 
     [SerializeField]
-    private GameObject findRadius;
+    private FindResource findRadius;
 
     [SerializeField]
     public SendLog sendLog;
@@ -24,38 +24,33 @@ public class FindResource : MonoBehaviour {
     void Update() {
         
         if (foundResource) {
-            findRadius.GetComponent<LookForResource>().setMove(false);
+            findRadius.GetComponent<FindResource>().setMove(false);
             if (period > 1f)
             {
-                if (gameObjects[resourceCount - 1] != null)
+                if (gameObjects[gameObjects.Count - 1] != null)
                 {
-                    gameObjects[resourceCount - 1].SendMessage("decreaseResourceAssets", assetDecreaser);
+                    gameObjects[gameObjects.Count - 1].SendMessage("decreaseResourceAssets", assetDecreaser);
                 }
                 else
                 {
-                    resourceLost(gameObjects[resourceCount - 1]);
+                    resourceLost(gameObjects[gameObjects.Count - 1]);
                 }
                 period = 0;
             }
             period += UnityEngine.Time.deltaTime;  
         }
     }
-    
-        private void OnTriggerEnter(Collider collInfo)
-    {
+
+    private void OnTriggerEnter(Collider collInfo) {
         
-        if (collInfo.name == "Resource")
-        {
+        if (collInfo.name == "Resource") {
             sendLog.msgCasual("Human Found a Resource:" + collInfo.name);
             sendLog.msgCasual("Resource:" + collInfo.gameObject.name);
 
-            MeshRenderer meshRend = GetComponent<MeshRenderer>();
-            meshRend.material.color = Color.blue;
-            resourceCount++;
+            changeRadiusColor(Color.blue);
             foundResource = true;
             gameObjects.Add(collInfo.gameObject);
-
-
+            
             parentUnit.GetComponent<MovementController>().setEndPosition(parentUnit.transform.position);
             parentUnit.GetComponent<MovementController>().setMoving();
         }
@@ -71,16 +66,19 @@ public class FindResource : MonoBehaviour {
     }
 
     private void resourceLost(GameObject gameObject) {
+
         sendLog.msgCasual("Resource Lost ");
-        MeshRenderer meshRend = GetComponent<MeshRenderer>(); ;
-        gameObjects.RemoveAt(resourceCount-1);
-        resourceCount--;
-        if (resourceCount == 0)
-        {
-            meshRend.material.color = Color.green;
+        gameObjects.RemoveAt(gameObjects.Count -1);
+
+        if (gameObjects.Count.Equals(0)) {
+            changeRadiusColor(Color.green);
             foundResource = false;
-            findRadius.GetComponent<LookForResource>().setMove(true);
+            findRadius.GetComponent<FindResource>().setMove(true);
         }
+    }
+
+    private void changeRadiusColor(Color color) {
+        gameObject.GetComponent<MeshRenderer>().material.color = color;
     }
 
 }
